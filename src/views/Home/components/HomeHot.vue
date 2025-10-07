5661e073d068fbd8a97d05a7a6fff643bfcaf639
vo50 <2506694372@qq.com>
2025-10-07 21:05:59 +0800

    home-人气推荐业务实现

--- src/views/Home/components/HomeHot.vue
+++ src/views/Home/components/HomeHot.vue
@@ -1,3 +1,71 @@
+<script setup>
+import HomePanel from './HomePanel.vue'
+import { findHotAPI } from '@/apis/home'
+import { onMounted, ref } from 'vue'
+const hotList = ref([])
+const getHotList = async () => {
+  const ref = await findHotAPI()
+  console.log(ref)
+  hotList.value = ref.result
+}
+onMounted(() => {
+  getHotList()
+})
+</script>
+
 <template>
-  我是热门
+  <HomePanel title="新鲜好物" sub-title="新鲜出炉 品质靠谱">
+    <!-- 下面是插槽主体内容模版 -->
+    <ul class="goods-list">
+      <li v-for="item in hotList" :key="item.id">
+        <RouterLink to="/">
+          <img :src="item.picture" alt="" />
+          <p class="name">{{ item.title }}</p>
+          <p class="price">{{ item.alt }}</p>
+        </RouterLink>
+      </li>
+    </ul>
+
+  </HomePanel>
+
 </template>
+
+
+<style scoped lang='scss'>
+.goods-list {
+  display: flex;
+  justify-content: space-between;
+  height: 406px;
+
+  li {
+    width: 306px;
+    height: 406px;
+
+    background: #f0f9f4;
+    transition: all .5s;
+
+    &:hover {
+      transform: translate3d(0, -3px, 0);
+      box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
+    }
+
+    img {
+      width: 306px;
+      height: 306px;
+    }
+
+    p {
+      font-size: 22px;
+      padding-top: 12px;
+      text-align: center;
+      text-overflow: ellipsis;
+      overflow: hidden;
+      white-space: nowrap;
+    }
+
+    .price {
+      color: $priceColor;
+    }
+  }
+}
+</style>
