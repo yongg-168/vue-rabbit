2c1e92fa2b171545160547e1b049617aae2e0761
vo50 <2506694372@qq.com>
2025-10-07 19:29:25 +0800

    Home-整体结构拆分以及分类的数据渲染

---
+++ src/views/Home/components/HomeBanner.vue
@@ -0,0 +1,3 @@
+<template>
+  我是板块
+</template>
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,9 +1,17 @@
 <template>
-  我是home页面
-  <div class="test"></div>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProducts />
 </template>
-<style scoped>
-.test {
-  height: 1200px;
-}
-</style>
+<script setup>
+import HomeCategory from './components/HomeCategory.vue'
+import HomeBanner from './components/HomeBanner.vue'
+import HomeNew from './components/HomeNew.vue'
+import HomeHot from './components/HomeHot.vue'
+import HomeProducts from './components/HomeProducts.vue'
+</script>
+<style scoped></style>
