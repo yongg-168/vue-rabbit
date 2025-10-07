33d7cecbc89f21533b9383261baf213e6004d3b3
vo50 <2506694372@qq.com>
2025-10-07 20:43:59 +0800

    home-面板组件封装

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -6,6 +6,12 @@
   <HomeNew />
   <HomeHot />
   <HomeProducts />
+  <HomePanel title="新鲜好物" sub-title="新鲜好物 好多商品">
+    <div>我是新鲜好物的内容</div>
+  </HomePanel>
+  <HomePanel title="人气推荐" sub-title="人气推荐 好多商品">
+    <div>我是人气推荐的内容</div>
+  </HomePanel>
 </template>
 <script setup>
 import HomeCategory from './components/HomeCategory.vue'
@@ -13,5 +19,6 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import HomeProducts from './components/HomeProducts.vue'
+import HomePanel from './components/HomePanel.vue'
 </script>
 <style scoped></style>
