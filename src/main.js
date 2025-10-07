c2cd53c1ff2f981796d29b5903f0fb7942cc167e
vo50 <2506694372@qq.com>
2025-10-07 13:45:42 +0800

    补交

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import '@/styles/common.scss'
 //测试接口函数
 import { getCategory } from './apis/testApi'
 getCategory().then(res => {
