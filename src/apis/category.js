e8d87da0a2e1ca7d3e1b144b9508c3291ab55416
vo50 <2506694372@qq.com>
2025-10-08 21:34:04 +0800

    二级分类-面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -1,7 +1,17 @@
 import httpInstance from "@/utils/http"
+//一级分类：面包屑导航
 export const getCategoryAPI = (id) => {
   return httpInstance({
     url: '/category',
+    params: {
+      id
+    }
+  })
+}
+//二级分类面包屑导航
+export const getCategoryFilterAPI = (id) => {
+  return httpInstance({
+    url: '/category/sub/filter',
     params: {
       id
     }
