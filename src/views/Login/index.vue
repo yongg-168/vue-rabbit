93bf66fa6b64faf75f9d08778870473da311db6e
vo50 <2506694372@qq.com>
2025-10-11 18:21:34 +0800

    登录-表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,20 @@
 <script setup>
-
+import { ref } from 'vue'
+//1.准备表单对象
+const form = ref({
+  account: '',
+  password: ''
+})
+//2.准备规则对象
+const rules = ref({
+  account: [
+    { required: true, message: '用户名不能为空值', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '密码不能为空值', trigger: 'blur' },
+    { min: 6, max: 14, message: '请输入6到14位的密码', trigger: 'blur' }
+  ]
+})
 </script>
 
 
@@ -24,12 +39,12 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form :model="form" :rules="rules" label-position="right" label-width="60px" status-icon>
+              <el-form-item label="账户" prop="account">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item label="密码" prop="password">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large">
