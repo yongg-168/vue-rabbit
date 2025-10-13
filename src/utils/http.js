23ddd42870f39b3f3fa8d7ac25f83767e7ae4bac
vo50 <2506694372@qq.com>
2025-10-13 16:21:41 +0800

    401-拦截处理

--- src/utils/http.js
+++ src/utils/http.js
@@ -3,6 +3,7 @@ import axios from "axios"
 import { ElMessage } from 'element-plus'
 import 'element-plus/theme-chalk/el-message.css'
 import { useUserStore } from "@/stores/user"
+import router from '@/router/index'
 const httpInstance = axios.create({
   baseURL: 'http://pcapi-xiaotuxian-front-devtest.itheima.net',
   // timeout: 5000
@@ -22,8 +23,16 @@ httpInstance.interceptors.request.use(config => {
 //响应拦截器
 httpInstance.interceptors.response.use(res => res.data,
   e => {
+    const userStore = useUserStore()
     //统一错误提示
     ElMessage.error(e.response.data.message)
+    //401 token失效处理
+    if (e.response.status === 401) {
+      //1.清除本地用户数据
+      userStore.clearUserInfo()
+      //2.返回至登录页
+      router.replace('/Login')
+    }
     return Promise.reject(e)
   })
 export default httpInstance
--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -43,7 +43,7 @@ const doLogin = () => {
   //调用实例方法
   formRef.value.validate(async (valid) => {
     //volid :所有项，表单都通过校验，才为true
-    console.log(valid)
+    // console.log(valid)
     //以valid作为判断条件 如果通过校验才执行登录逻辑
     if (valid) {
       //TODO LOGIN
