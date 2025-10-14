7a8cb2caa52a90a9066b4aa8c8c21a5d812e41b5
vo50 <2506694372@qq.com>
2025-10-14 18:45:41 +0800

    接口购物车-退出清除数据-登录获取数据

--- src/stores/cartStore.js
+++ src/stores/cartStore.js
@@ -34,7 +34,10 @@ export const useCartStore = defineStore('cart', () => {
       }
     }
   }
-
+  //清除购物车
+  const clearCart = () => {
+    cartList.value = []
+  }
   //删除购物车
   const deleteCart = async (skuId) => {
     if (isToken.value) {
@@ -81,9 +84,11 @@ export const useCartStore = defineStore('cart', () => {
     selectedCount,
     selectedPrice,
     addCart,
+    clearCart,
     deleteCart,
     singleCheck,
-    allCheck
+    allCheck,
+    updateNewList
   }
 }, {
   persist: true
--- src/stores/user.js
+++ src/stores/user.js
@@ -2,18 +2,24 @@
 import { defineStore } from "pinia"
 import { ref } from "vue"
 import { loginAPI } from "@/apis/user"
+import { useCartStore } from "./cartStore"
 export const useUserStore = defineStore('user', () => {
+  const cartStore = useCartStore()
   //1.定义管理用户数据的state
   const userInfo = ref({})
   //2.定义获取接口的action函数
   const getUserInfo = async ({ account, password }) => {
     const res = await loginAPI({ account, password })
     userInfo.value = res.result
+    //登陆时，获取购物车信息
+    cartStore.updateNewList()
   }
 
   //退出登录时，清除pinia数据
   const clearUserInfo = () => {
     userInfo.value = {}
+    //退出时，清理购物车
+    cartStore.clearCart()
   }
   //3.以对象的形式把state和action返回出去
   return {
